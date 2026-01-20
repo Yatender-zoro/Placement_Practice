cc7ee5bf7032df8844db649a04b8b64796178ec2
Yatender-zoro <yatendar.com@gmail.com>
2026-01-20 18:46:09 +0530

    Time: 21 ms (48.05%), Space: 44.6 MB (25.06%) - LeetHub

--- 1395-count-number-of-teams/1395-count-number-of-teams.java
+++ 1395-count-number-of-teams/1395-count-number-of-teams.java
@@ -1,14 +1,37 @@
 class Solution {
-    public int numTeams(int[] ratings) {
-        int n = ratings.length;
+    public int numTeams(int[] rating) {
+        int n = rating.length;
         int c = 0;
-        for(int i=0;i<n-2;i++){
-            for(int j=i+1;j<n-1;j++){
-                boolean f = ratings[i]<ratings[j];
-                for(int k=j+1;k<n;k++){
-                    if((ratings[k]>ratings[j])==f) c++;
+        // for(int i=0;i<n-2;i++){
+        //     for(int j=i+1;j<n-1;j++){
+        //         boolean f = ratings[i]<ratings[j];
+        //         for(int k=j+1;k<n;k++){
+        //             if((ratings[k]>ratings[j])==f) c++;
+        //         }
+        //     }
+        // }
+        // return c;
+
+        for(int i=0;i<n;i++){ // i is for middle element
+            int lefts=0,leftg=0;
+            int rights=0,rightg = 0;
+
+            for(int j=0;j<n;j++){
+                if(j<i){
+                    if(rating[j]<rating[i]){
+                        lefts++;
+                    }
+                    else leftg++;
+                }
+                else if(j>i){
+                    if(rating[j]>rating[i]){
+                        rightg++;
+                    }
+                    else rights++;
                 }
             }
+            int total= lefts*rightg + leftg*rights;
+            c+=total;
         }
         return c;
     }
