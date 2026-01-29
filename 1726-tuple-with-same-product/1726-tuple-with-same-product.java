e276886fd0e52310f4baf1bafca36866c824e49e
Yatender <yatendar.com@gmail.com>
2026-01-29 21:09:08 +0530

    Time: 177 ms (69.5%), Space: 107.6 MB (36.88%) - LeetHub

--- 1726-tuple-with-same-product/1726-tuple-with-same-product.java
+++ 1726-tuple-with-same-product/1726-tuple-with-same-product.java
@@ -1,20 +1,20 @@
 class Solution {
     public int tupleSameProduct(int[] nums) {
         int n = nums.length;
-        int pairs = 0;
-        HashMap<Integer,Integer> map = new HashMap<>();
+        Map<Integer,Integer> map = new HashMap<>();
+        int cnt=0;
 
-        for(int i=0;i<n;i++){
-            for(int j=i+1;j<n;j++){
-                int p = nums[i]*nums[j];
-                map.put(p,map.getOrDefault(p,0)+1);
+        for(int i =0; i<n; i++){
+            for(int j =i+1; j< n; j++){
+                int prod = nums[i] * nums[j];
+                if(map.containsKey(prod)){
+                    cnt += ( map.get(prod) * 8) ;
+                    map.put(prod, map.get(prod)+1);
+                }else{
+                    map.put(prod,1);
+                }
             }
         }
-
-        for(Integer x:map.keySet()){
-            int f = map.get(x);
-            pairs+=(f*(f-1)/2);
-        }
-        return pairs*8;
+    return cnt;
     }
 }
