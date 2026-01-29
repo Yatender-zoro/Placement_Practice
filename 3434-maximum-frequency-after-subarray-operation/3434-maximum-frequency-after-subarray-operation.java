9d0ad85b7ea7c80dc9628fc94b1867c678a0b78d
Yatender <yatendar.com@gmail.com>
2026-01-29 21:23:09 +0530

    Time: 2 ms (100%), Space: 83.4 MB (59.46%) - LeetHub

--- 3434-maximum-frequency-after-subarray-operation/3434-maximum-frequency-after-subarray-operation.java
+++ 3434-maximum-frequency-after-subarray-operation/3434-maximum-frequency-after-subarray-operation.java
@@ -1,33 +1,51 @@
 class Solution {
     public int maxFrequency(int[] nums, int k) {
-       int n = nums.length;
-        int base = 0;
-        for (int x : nums) {
-            if (x == k) base++;
-        }
-        HashSet<Integer> set = new HashSet<>();
-        for (int x : nums) {
-            if (x != k) set.add(x);
-        }
 
-        int bestGain = 0;
+        int dp[] = new int[51];
+        int countK = 0;
+        int max = 0;
 
-        for (int val : set) {
-            int curr = 0;
-            int maxHere = 0;
-
-            for (int x : nums) {
-                int score = 0;
-                if (x == val) score = 1;
-                else if (x == k) score = -1;
-
-                curr = Math.max(score, curr + score);
-                maxHere = Math.max(maxHere, curr);
+        for (int x: nums) {
+            if (x == k) {
+                countK++;
+                dp[x] = max + 1;
+            } else {
+                dp[x] = Math.max(dp[x] + 1, countK + 1);
             }
 
-            bestGain = Math.max(bestGain, maxHere);
+            max = Math.max(max, dp[x]);
         }
+        
+        return max;
+    //    int n = nums.length;
+    //     int base = 0;
+    //     for (int x : nums) {
+    //         if (x == k) base++;
+    //     }
+    //     HashSet<Integer> set = new HashSet<>();
+    //     for (int x : nums) {
+    //         if (x != k) set.add(x);
+    //     }
 
-        return base + bestGain;
+    //     int bestGain = 0;
+
+    //     for (int val : set) {
+    //         int curr = 0;
+    //         int maxHere = 0;
+
+    //         for (int x : nums) {
+    //             int score = 0;
+    //             if (x == val) score = 1;
+    //             else if (x == k) score = -1;
+
+    //             curr = Math.max(score, curr + score);
+    //             maxHere = Math.max(maxHere, curr);
+    //         }
+
+    //         bestGain = Math.max(bestGain, maxHere);
+    //     }
+
+    //     return base + bestGain;
     }
+
 }
